ba4f4c804bef2296f53f590eb7104c1a4110aefc
Ariel Shahrur <ariel02888@gmail.com>
2023-07-15 17:37:39 +0300

    add login

--- src/Gallery.tsx
+++ src/Gallery.tsx
@@ -6,7 +6,7 @@ import GalleryImgCard from './cardComponants/GalleryImgCard';
 function Gallery() {
   const { data }: { data: PlantIntrfc[] } = useFetchData();
 console.log(useFetchDataFromServer().data[0]);
-const tempArr=useFetchDataFromServer().data[0];
+// const tempArr=useFetchDataFromServer().data[0];
 
   return (
     <div className='galleryDiv'>
--- src/Login.tsx
+++ src/Login.tsx
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import Cookies from 'js-cookie';
+
 import './login.css'
 
 function Login() {
@@ -6,22 +8,33 @@ function Login() {
         username: '',
         email: '',
         password:'',
-        configpassword:''
+        confirmPassword:''
       });
+      const [loginData, setLoginData] = useState({
+        username: '',
+        password:''
+      });
+
+
       const handleForChangeEvent = (event:React.ChangeEvent<HTMLInputElement>) => {
         setFormData({
           ...formData,
           [event.target.name]: event.target.value,
         });
         console.log(formData);
-        
+      };
+      const handleForChangeEventLogin = (event:React.ChangeEvent<HTMLInputElement>) => {
+        setLoginData({
+          ...loginData,
+          [event.target.name]: event.target.value,
+        });
+        console.log(loginData);
       };
 
-      const handleSubmit = (event:React.FormEvent<HTMLButtonElement>) => {
+      const handleSubmit = (event:React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-      
         // Send the POST request
-        fetch('http://127.0.0.1:3001/postExempal', {
+        fetch('http://127.0.0.1:3005/signup', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -39,9 +52,28 @@ function Login() {
           });
       };
     
+      const handleLogin = (event:React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        // Send the POST request
+        fetch('http://127.0.0.1:3005/login', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(loginData),
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            Cookies.set('token', data.token);
+          })
+          .catch((error) => {
+            // Handle errors
+            console.error(error);
+          });
+      };
   return (
     <div>
-        <form action="http://127.0.0.1:3001/postExempal" method='post'>
+        <form onSubmit={(e)=>{handleSubmit(e)}}>
             <label  htmlFor="">User Name:
             <input type="text" placeholder='User Name' name='username' onChange={(e)=>{handleForChangeEvent(e)}} />
             </label>
@@ -52,9 +84,19 @@ function Login() {
             <input type="text" placeholder='password' name='password'onChange={(e)=>{handleForChangeEvent(e)}} />
             </label>
             <label htmlFor="">Config Password:
-            <input type="text" placeholder=" config you'r password" name='configpassword' onChange={(e)=>{handleForChangeEvent(e)}}/>
+            <input type="text" placeholder=" config you'r password" name='confirmPassword' onChange={(e)=>{handleForChangeEvent(e)}}/>
             </label>
-            <button type = 'submit' onSubmit={(e)=>{handleSubmit(e)}}>submit</button>
+            <button type = 'submit'>submit</button>
+        </form>
+
+        <form onSubmit={(e)=>{handleLogin(e)}}>
+            <label  htmlFor="">User Name:
+            <input type="text" placeholder='User Name' name='username' onChange={(e)=>{handleForChangeEventLogin(e)}} />
+            </label>
+            <label htmlFor="">Password:
+            <input type="text" placeholder='password' name='password'onChange={(e)=>{handleForChangeEventLogin(e)}} />
+            </label>
+            <button type = 'submit'>log in</button>
         </form>
     </div>
   )
